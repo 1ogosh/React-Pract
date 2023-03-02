acb92a2bb3ccd9efc4e2a083cbfd7dde1e646773
Andrew Salmanov <ads739912@gmail.com>
2023-03-02 10:47:53 +0200

    solved increase problem

--- src/components/employers-list-item/employers-list-item.js
+++ src/components/employers-list-item/employers-list-item.js
@@ -1,17 +1,17 @@
 import './employers-list-item.scss';
 
-const EmployersListItem = ({name, salery, increase}) => {
-    
+const EmployersListItem = ({ name, salery, increase }) => {
+
     let classNames = 'app-list-group-item';
     if (increase) {
-        classNames += ' -increase';
+        classNames += ' increase';
     }
-    
+
     return (
         <li className={classNames}>
 
             <span className="app-list-group-item-label">{name}</span>
-            <input type="text" className="app-list-group-item-input" defaultValue={salery + '$'}/>
+            <input type="text" className="app-list-group-item-input" defaultValue={salery + '$'} />
             <div className>
                 <button type="button" className="btn-cookie"></button>
 

dc92e5cc2bd8aef14b13350b4d5995d447457dfa
Andrew Salmanov <ads739912@gmail.com>
2023-03-03 19:24:30 +0200

    state-changing and some debugging

--- src/components/employers-list-item/employers-list-item.js
+++ src/components/employers-list-item/employers-list-item.js
@@ -1,7 +1,25 @@
+import { Component } from 'react';
 import './employers-list-item.scss';
 
-const EmployersListItem = ({ name, salery, increase }) => {
 
+class EmployersListItem extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            increase: false
+        }
+    }
+    
+    onIncrease = () => {
+        this.setState(({increase}) => ({
+            increase: !increase
+        }));
+    }
+    
+  render() {
+    const {name, salery} = this.props;
+    const {increase} = this.state;
+    
     let classNames = 'app-list-group-item';
     if (increase) {
         classNames += ' increase';
@@ -9,19 +27,24 @@ const EmployersListItem = ({ name, salery, increase }) => {
 
     return (
         <li className={classNames}>
-
             <span className="app-list-group-item-label">{name}</span>
             <input type="text" className="app-list-group-item-input" defaultValue={salery + '$'} />
-            <div className>
-                <button type="button" className="btn-cookie"></button>
+            <div>
+                <button type="button" 
+                        className="btn-cookie" 
+                        onClick={this.onIncrease}>            
+                </button>
 
-                <button type="button" className="btn-trash"></button>
+                <button type="button" 
+                        className="btn-trash">
+                </button>
 
             </div>
 
         </li>
 
     )
+  }
 }
 
 export default EmployersListItem;

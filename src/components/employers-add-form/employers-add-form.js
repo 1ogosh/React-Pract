bc245679e3576759ec6c03a9d9d8220dd948b97a
Andrew Salmanov <ads739912@gmail.com>
2023-03-05 14:38:57 +0200

    form input control

--- src/components/employers-add-form/employers-add-form.js
+++ src/components/employers-add-form/employers-add-form.js
@@ -1,16 +1,48 @@
+import { Component } from 'react';
+
 import './employers-add-form.scss';
 
-const EmployersAddForm = () => {
-        return (
-                <div className="form">
-                        <h3>Добавьте нового сотрудника</h3>
-                        <form className="add-form">
-                                <input type="text" className="add-form-input" placeholder="Как его зовут?" />
-                                <input type="number" className="add-form-input" placeholder="З/П в $" />
-                                <button type="submit" className="add-form-btn">Добавить</button>
-                        </form>
-                </div>
-        )
+class EmployersAddForm extends Component {
+        constructor(props) {
+                super(props);
+                this.state = {
+                        name: '',
+                        salary: ''
+                }
+        }
+
+        onValueChange = (e) => {
+                this.setState({
+                        [e.target.name] : e.target.value
+                })
+        }
+
+        render() {
+                
+                const {name, salary} = this.state;
+                
+                return (
+                        <div className="form">
+                                <h3>Добавьте нового сотрудника</h3>
+                                <form className="add-form">
+                                        <input type="text"
+                                                className="add-form-input"
+                                                placeholder="Как его зовут?"
+                                                name="name"
+                                                value={name}
+                                                onChange={this.onValueChange} />
+                                        <input type="number"
+                                                className="add-form-input"
+                                                placeholder="З/П в $"
+                                                name="salary"
+                                                value={salary}
+                                                onChange={this.onValueChange} />
+                                        <button type="submit"
+                                                className="add-form-btn">Добавить</button>
+                                </form>
+                        </div>
+                )
+        }
 }
 
 export default EmployersAddForm;
--- src/components/employers-list-item/employers-list-item.js
+++ src/components/employers-list-item/employers-list-item.js
@@ -2,7 +2,7 @@ import { Component } from 'react';
 import './employers-list-item.scss';
 
 
-class EmployersListItem extends Component{
+class EmployersListItem extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -10,58 +10,58 @@ class EmployersListItem extends Component{
             like: false
         }
     }
-    
+
     onIncrease = () => {
-        this.setState(({increase}) => ({
+        this.setState(({ increase }) => ({
             increase: !increase
         }));
     }
-    
+
     onLike = () => {
-        this.setState(({like}) => ({
-           like: !like 
+        this.setState(({ like }) => ({
+            like: !like
         }));
     }
-    
-  render() {
-    const {name, salery} = this.props;
-    const {increase} = this.state;
-    const {like} = this.state;
-    
-    let classNames = 'app-list-group-item';
-    if (increase) {
-        classNames += ' increase';
-    }
-    
-    if (like) {
-        classNames += ' like';
-    }
 
-    return (
-        <li className={classNames}>
-            <span className="app-list-group-item-label" onClick={this.onLike}>{name}</span>
-            <input type="text" className="app-list-group-item-input" defaultValue={salery + '$'} />
-            <div>
-                <button type="button"
-                    className="btn-like"
-                    onClick={this.onLike}>
-                </button>
-                
-                <button type="button" 
-                        className="btn-cookie" 
-                        onClick={this.onIncrease}>            
-                </button>
+    render() {
+        const { name, salery } = this.props;
+        const { increase } = this.state;
+        const { like } = this.state;
 
-                <button type="button" 
+        let classNames = 'app-list-group-item';
+        if (increase) {
+            classNames += ' increase';
+        }
+
+        if (like) {
+            classNames += ' like';
+        }
+
+        return (
+            <li className={classNames}>
+                <span className="app-list-group-item-label" onClick={this.onLike}>{name}</span>
+                <input type="text" className="app-list-group-item-input" defaultValue={salery + '$'} />
+                <div>
+                    <button type="button"
+                        className="btn-like"
+                        onClick={this.onLike}>
+                    </button>
+
+                    <button type="button"
+                        className="btn-cookie"
+                        onClick={this.onIncrease}>
+                    </button>
+
+                    <button type="button"
                         className="btn-trash">
-                </button>
+                    </button>
 
-            </div>
+                </div>
 
-        </li>
+            </li>
 
-    )
-  }
+        )
+    }
 }
 
 export default EmployersListItem;
